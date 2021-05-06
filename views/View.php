d0bdff0412359d4b12eb1e45ab7acb041787bda9
Michel Descotes <micheldescotes65@gmail.com>
2021-05-06 10:51:00 +0200

    mise a jour

--- views/View.php
+++ views/View.php
@@ -12,20 +12,22 @@ class View
 
   function __construct($action)
   {
-    $this->_file = 'views/view'.$action.'.php';
+    $this->_file = 'views/view' . $action . '.php';
   }
 
   //Create function generate and display the view
-  public function generate($data){
+  public function generate($data)
+  {
     //content send
     $content = $this->generateFile($this->_file, $data);
 
-    //TEMPLATE
+
     $view = $this->generateFile('views/template.php', array('t' => $this->_t, 'content' => $content));
     echo $view;
   }
 
-  private function generateFile($file, $data){
+  private function generateFile($file, $data)
+  {
     if (file_exists($file)) {
       extract($data);
 
@@ -35,33 +37,9 @@ class View
       require $file;
 
       //stop the temporisation
-     return ob_get_clean();
-    }
-    else {
-      throw new \Exception("Fichier ".$file." introuvable", 1);
-
+      return ob_get_clean();
+    } else {
+      throw new \Exception("Fichier " . $file . " introuvable", 1);
     }
   }
-
-
-
-
-
-
-
-
-
-
-
-
 }
-
-
-
-
-
-
-
-
-
- ?>
