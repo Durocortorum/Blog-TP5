a88ca2bf977a5ba8c7406901642e038af5ddaacb
Michel Descotes <micheldescotes65@gmail.com>
2021-05-08 09:38:30 +0200

    create folder InscriptionManager.php in the models folder

---
+++ models/InscriptionManager.php
@@ -0,0 +1,10 @@
+<?php
+
+class InscriptionManager extends Model
+{
+  //FONCTION D'INSCRIPTION
+  public function inscription($email, $nom, $prenom, $password){
+    return $this->new_user($email, $nom, $prenom, $password);
+  }
+}
+ ?>
