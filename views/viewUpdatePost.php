9b0d2314abb7dd31f474851ac0f3ff8f399fa3df
Michel Descotes <micheldescotes65@gmail.com>
2021-05-15 11:48:35 +0200

    layout viewUpdatePost.php

--- views/viewUpdatePost.php
+++ views/viewUpdatePost.php
@@ -1,37 +1,36 @@
 <div class="slider">
-    <div class="display-table  center-text">
-        <h1 class="title display-table-cell"></h1>
-    </div>
+
 </div>
 
-<section style="width:80%">
+<section class="blog-area section">
+    <h2>Modifier Post</h2><br />
+    <h3><?= $post->title() ?></h3><br />
+    <h5><?= $post->author() ?></h3>
+        <h6>Modifié le <?= $post->date() ?></h4><br>
 
-    <h1><b><?= $post->title() ?></b></h2>
-        <h5><?= $post->author() ?></h3>
-            <h6>Modifié le <?= $post->date() ?></h4><br>
 
-                <h2>Modifier Post</h2>
-                <div class="form-group">
-                    <form method="post" action="Post&update=1&id=<?= $_GET['id'] ?>">
-                        <?php if ($form == 1) {
+            <div class="form-group">
+                <form method="post" action="Post&update=1&id=<?= $_GET['id'] ?>">
+                    <?php if ($form == 1) {
 
-                        ?>
-                            <label for="title">Titre:</label>
-                            <input type="text" class="form-control" placeholder="Titre du Post" value="<?= $post->title() ?>" name="title" style="width:40%"><br>
+                    ?>
+                        <label for="title">Titre:</label><br/>
+                        <input class="text-center" type="text" class="form-control" placeholder="Titre du Post" value="<?= $post->title() ?>" name="title" style="width:40%"><br><br/>
 
-                            <label for="chapo">Chapô:</label>
-                            <textarea name="chapo" class="form-control" style="width:65%" rows="15"><?= $post->chapo(); ?></textarea><br>
+                        <label for="chapo">Chapô:</label><br/>
+                        <textarea class="text-center" name="chapo" class="form-control" style="width:65%" rows="15"><?= $post->chapo(); ?></textarea><br><br/>
 
-                            <label for="content">Contenu:</label>
-                            <textarea name="content" class="form-control" style="width:75%" rows="25"><?= $post->content(); ?></textarea><br>
+                        <label for="content">Contenu:</label><br/>
+                        <textarea class="text-center" name="content" class="form-control" style="width:75%" rows="25"><?= $post->content(); ?></textarea><br>
 
-                            <input type="hidden" name="date" value="<?= date("Y-m-d H:i:s"); ?>">
-                            <input type="hidden" name="updatePost" value="true">
-                            <input type="submit" value="OK">
+                        <input type="hidden" name="date" value="<?= date("Y-m-d H:i:s"); ?>">
+                        <input type="hidden" name="updatePost" value="true"><br/>
 
-                        <?php
-                        }
-                        ?>
-                    </form>
+                        <input class="btn btn-primary"type="submit" value="Modifier">
+
+                    <?php
+                    }
+                    ?>
+                </form>
 
 </section>
