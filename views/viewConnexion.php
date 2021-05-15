66bf50f70bb926ab7d1d1c5a7837c3ad289ab36d
Michel Descotes <micheldescotes65@gmail.com>
2021-05-15 11:30:25 +0200

    layout viewConnexion.php

--- views/viewConnexion.php
+++ views/viewConnexion.php
@@ -1,21 +1,23 @@
 <div class="slider"></div>
 
-<section class="blog-area section">
+<section class="blog-area  section">
     <div class="container">
-        <h2>Espace Connexion</h2>
+        <h2 class="text-center">Espace Connexion</h2>
+        <br />
         <div class="form-group">
             <form method="post" action="Connexion">
                 <?php if ($connexion[0] == "retryForm") {
                     echo "Identifiants introuvables<br>";
                 }
                 ?>
-                <label for="email">Email:</label>
-                <input class="lg-12" type="email" class="form-control" placeholder="Email" name="email" style="width:40%">
+                <label class="col-3 text-right" for="email">Email:</label>
+                <input class="col-12" type="email" class="form-control" placeholder="Email" name="email" style="width:40%">
                 <br />
-                <label for="password">Mot de passe:</label>
-                <input class="lg-12" type="password" class="form-control" placeholder="Mot de passe" name="password" style="width:40%">
-
-                <br /><input type="submit" value="OK">
+                <label class="col-3 text-right" for="password">Mot de passe:</label>
+                <input class="col-12" type="password" class="form-control" placeholder="Mot de passe" name="password" style="width:40%">
+                <br />
+                <br />
+                <input class="col-4 btn btn-primary" type="submit" name="form_button" value="OK">
             </form>
         </div>
 </section>
