6783e735f906a12bbf3d59eb8f6ae4e3f33307da
Michel Descotes <micheldescotes65@gmail.com>
2021-05-19 18:39:03 +0200

    modif in View.php

--- views/View.php
+++ views/View.php
@@ -15,16 +15,9 @@ class View
     $this->_file = 'views/view' . $action . '.php';
   }
 
-<<<<<<< HEAD
-  //Create function generate and display the view
-  public function generate($data)
-  {
-    //content send
-=======
   //CREATION D'UNE FONCTION QUI VA GENERER ET AFFICHER LA VUE
   public function generate($data){
     //DEFINITION DU CONTENU A ENVOYER
->>>>>>> dev
     $content = $this->generateFile($this->_file, $data);
 
 
@@ -32,13 +25,8 @@ class View
     echo $view;
   }
 
-<<<<<<< HEAD
-  private function generateFile($file, $data)
-  {
-=======
 
   private function generateFile($file, $data){
->>>>>>> dev
     if (file_exists($file)) {
       extract($data);
 
@@ -47,15 +35,6 @@ class View
 
       require $file;
 
-<<<<<<< HEAD
-      //stop the temporisation
-      return ob_get_clean();
-    } else {
-      throw new \Exception("Fichier " . $file . " introuvable", 1);
-    }
-  }
-}
-=======
       //arreter la temporisation
      return ob_get_clean();
     }
@@ -65,5 +44,3 @@ class View
     }
   }
 }
-
->>>>>>> dev
